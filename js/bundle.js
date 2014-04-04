52a29f6c13ccc6020e04d905f25ccb0642f0d523
Matias Figueroa <mfigueroa@salesforce.com>
2014-04-04 18:19:42 -0300

    Publish Fri Apr  4 18:19:42 ART 2014

--- js/bundle.js
+++ js/bundle.js
@@ -8097,4 +8097,47 @@ CodeMirror.defineMIME("text/html", "htmlmixed");
         }
     });
 
-})(CodeMirror);
+})(CodeMirror);;;document.querySelector && (function (container) {
+  var icons = Array.prototype.slice.call(container.querySelectorAll(".icons i[class*=icon-]")),
+      colors = Array.prototype.slice.call(container.querySelectorAll(".colors .color")),
+      sample = container.querySelector(".sample"),
+      code;
+
+  getCMEditor(container.querySelector(".code"), function(editor){
+    code = editor;
+  });
+
+  var currentIcon = "",
+      currentColor = "";
+
+  function updateSample() {
+    [1,2,4].forEach(function (size) {
+      var i = sample.querySelector(".icon-"+size+"x");
+      i.className = currentIcon + " " +"icon-"+size+"x";
+      i.style.color = currentColor;
+    });
+
+    if(currentColor == "white") {
+      sample.style.backgroundColor = "black";
+    }
+    else {
+      sample.style.backgroundColor = "white";
+    }
+  }
+
+  icons.forEach(function(icon) {
+    icon.addEventListener("mouseover", function(){
+      code && code.setValue(icon.outerHTML);
+      currentIcon = icon.className;
+      updateSample();
+    });
+  });
+
+  colors.forEach(function(color) {
+    color.addEventListener("mouseover", function(e) {
+      currentColor = color.dataset.color;
+      updateSample();
+    });
+  });
+
+})(document.querySelector("#ri-icon-fonts"));
